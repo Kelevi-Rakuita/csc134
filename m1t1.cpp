3706e3183b3ca5757fd97f7e021b6cce15e6e5cc
Kelevi Rogica Rakuita <rakuitak1394@student.faytechcc.edu>
2025-08-25 15:04:26 +0000

    M1T1. successfully and greete user.

---
+++ m1t1.cpp
@@ -0,0 +1,20 @@
+/*
+m1+1 = hello, user
+Kelevi-Rakuita, 08/25/25
+INPUT: run the program
+PROCESS: None yet
+OUTPUT: return 0 (no error)
+SPECIAL FX:Greet the user
+*/
+
+// magic words to start
+#include <iostream>
+using namespace std;
+
+int main() {
+    //SFX: Greet user
+    cout << "Hello, user!" << endl;
+
+    // output
+    return 0; // no errors
+}
