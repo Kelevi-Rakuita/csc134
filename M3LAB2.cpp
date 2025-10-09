7ed0939d0866e0b0f1e48c320aff93b4c0411b41
Kelevi Rogica Rakuita <rakuitak1394@student.faytechcc.edu>
2025-10-09 16:18:21 +0000

    complete

--- M3LAB2.cpp
+++ M3LAB2.cpp
@@ -11,101 +11,99 @@ Rakuita
 
 using namespace std;
 
-// to make things easier , we'll write the code in a function
 // DECLARE the functions here
 void letter_grader();
 void combat();
+int roll();
 
 int main() {
-
+    // Seed random number generator
+   srand(time(0));
+    
     letter_grader();
     //combat();
-    int roll() {
-const int SIDES = 20;
-int my_roll;
-my_roll = (rand() % SIDES) + 1:
-return my_roll:
+    
     return 0;
 }
-// DEFINE the other function here
 
-void letter_grade() {
-// input a number grade
-// respond with a letter grade
-double num_grade;
-string letter_grade;
-const double A_GRADE = 90;
-const double B_GRADE = 80;
-const double C_GRADE = 70;
-const double D_GRADE = 60;
+// DEFINE the other functions here
 
-cout << "Enter a number grade 0-100:";
-cin >> num_grade;
+void letter_grader() {
+    // input a number grade
+    // respond with a letter grade
+    double num_grade;
+    string letter_grade;
+    const double A_GRADE = 90;
+    const double B_GRADE = 80;
+    const double C_GRADE = 70;
+    const double D_GRADE = 60;
 
-// Create the if statement) {
-if (num_grade >= A_GRADE) 
-    letter_grade = "A";
-}    
-else if (num_grade >= B_GRADE) {
-    letter_grade = "B";
-}
-else If (num_grade >= C_GRADE) {
-    letter_grade = "C";
-}
-else If (num_grade >= D_GRADE) {
-    letter_grade = "D";
-}
-else{
-    // must be under D...
-    letter_grade ="F";
-}
+    cout << "Enter a number grade 0-100: ";
+    cin >> num_grade;
 
-//Output the answer 
-cout << "A number grade of " << num_grade << " is a (n) " << letter_grade;
-cout << endl <<endl;
+    // Create the if statement
+    if (num_grade >= A_GRADE) {
+        letter_grade = "A";
+    }    
+    else if (num_grade >= B_GRADE) {
+        letter_grade = "B";
+    }
+    else if (num_grade >= C_GRADE) {
+        letter_grade = "C";
+    }
+    else if (num_grade >= D_GRADE) {
+        letter_grade = "D";
+    }
+    else {
+        // must be under D...
+        letter_grade = "F";
+    }
 
+    //Output the answer 
+    cout << "A number grade of " << num_grade << " is a(n) " << letter_grade;
+    cout << endl << endl;
 }
 
 
 void combat() {
     /*
     A simple D&D style combat demo
-    Attck roll + binus >= armor class? TThen hit, else miss
+    Attack roll + bonus >= armor class? Then hit, else miss
     */
     // variables
     int attck_roll, attck_bonus, enemy_armor;
     
     cout << "You are fighting a goblin." << endl;
-    cout << "Enter your roll:";
-    cin >> "attack_roll";
-    cout << "Enter attack_bonus:";
-    cin >> "attack Bonus";
-    cout << "Enemy armor calss; ";
+    cout << "Enter your roll: ";
+    cin >> attck_roll;
+    cout << "Enter attack bonus: ";
+    cin >> attck_bonus;
+    cout << "Enemy armor class: ";
     cin >> enemy_armor;
 
     
     // Roll to hit
-    cout << "Roll; " << attck_roll << attck_bonus << "=" << attck_roll+attck_bonus << endl;
+    cout << "Roll: " << attck_roll << " + " << attck_bonus << " = " << attck_roll + attck_bonus << endl;
     if (attck_roll + attck_bonus >= enemy_armor) {
-    cout << "Hit!" << endl;
+        cout << "Hit!" << endl;
     }
     else {
-    cout << "Miss!" << endl;
+        cout << "Miss!" << endl;
     }
     
     // try again?
-    cout << "Again? (y/n):";
+    cout << "Again? (y/n): ";
     string again;
     cin >> again;
-    if (again =="y"){
-    //just call the function again!
-    combat ();
+    if (again == "y") {
+        //just call the function again!
+        combat();
     }
 }
 
 int roll() {
-const int SIDES = 20;
-int my_roll;
-my_roll = (rand() % SIDES) + 1;
-return my_roll;
+    const int SIDES = 20;
+    int my_roll;
+    my_roll = (rand() % SIDES) + 1;
+    return my_roll;
 }
