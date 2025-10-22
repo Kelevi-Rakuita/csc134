193b8ae0aa2eaa261b25a9fd5312ba333e1680d7
Kelevi Rogica Rakuita <rakuitak1394@student.faytechcc.edu>
2025-10-22 14:56:42 +0000

    Fuction Definition

--- M5T1.cpp
+++ M5T1.cpp
@@ -13,17 +13,33 @@ void say_hello(); // says hi
 
 int get_the_answer(); // provides the naswer to everything
 
-double double_a_number(); // num times two
+double double_a_number(double); // num times two
 
 int main() {
     // This program does nothing useful
     double my_num;
     int another_num_base;
 
-    cout <<"Welcome to the best program ever!" << endl;
-    cout << "Please enter a number (with or without decimal place). " << endl;
+   cout << "Please enter a number (with or without decimal place). " << endl;
     cin >> my_num;
-    my_num = my_num * 2;
+    //my_num = my_num * 2;
+    my_num= double_a_number(my_num); //
     cout << "Double the number is: " << my_num<< endl;
 
+}
+
+//  Function Definitions (the whole function!) go here
+void say_hello() {
+    // says hi
+     cout <<"Welcome to the best program ever!" << endl;
+}
+
+int get_the_answer() {
+    //provides the answer to everything
+}
+
+double double_a_number(double the_num) {
+    // num times two
+    double answer = the_num * 2;
+    return answer;
 }
