fad3b9bfa5ccbce0c605f1b173788cbcf8a32089
Kelevi Rogica Rakuita <rakuitak1394@student.faytechcc.edu>
2025-10-22 14:48:45 +0000

    change

--- M5T1.cpp
+++ M5T1.cpp
@@ -11,19 +11,19 @@ using namespace std;
 // Functopns declarations (Definitions are at he bottom)
 void say_hello(); // says hi
 
-int get_the_answer(); // provides the naswer to everyhting
+int get_the_answer(); // provides the naswer to everything
 
 double double_a_number(); // num times two
 
 int main() {
     // This program does nothing useful
     double my_num;
-    int anohter_num_base;
+    int another_num_base;
 
     cout <<"Welcome to the best program ever!" << endl;
     cout << "Please enter a number (with or without decimal place). " << endl;
     cin >> my_num;
-    my_num * 2;
+    my_num = my_num * 2;
     cout << "Double the number is: " << my_num<< endl;
 
 }
